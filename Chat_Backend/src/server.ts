4e3e664c71aa50e759f4edc03a6be887a6e81541
JuancitoCH <cjuan.chona@hotmail.com>
2022-12-06 16:33:14 -0300

    ADD: Socket With not implementation yet

--- Chat_Backend/src/server.ts
+++ Chat_Backend/src/server.ts
@@ -1,5 +1,13 @@
 import app from "./app";
+import {Server} from 'socket.io'
+import http from 'http'
+const server = http.createServer(app)
+const io = new Server(server)
 
-app.listen(process.env.PORT,()=>{
+io.on('connection',(socket) => {
+    console.log('a user connected')
+})
+
+server.listen(process.env.PORT,()=>{
     console.log("- [Server] :  Listen on Port " + process.env.PORT)
 })
