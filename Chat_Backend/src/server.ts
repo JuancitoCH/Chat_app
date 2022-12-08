ba6f536a1f29161077b1aeb16d6e75629f294a3d
CJuan <cjuan.chona@hotmail.com>
2022-12-08 13:40:33 -0300

    ADD: Chat app in function with the socket example

--- Chat_Backend/src/server.ts
+++ Chat_Backend/src/server.ts
@@ -2,10 +2,24 @@ import app from "./app";
 import {Server} from 'socket.io'
 import http from 'http'
 const server = http.createServer(app)
-const io = new Server(server)
+
+const io = new Server(server,{
+    cors:{
+        origin:'*'
+    }
+})
+
 
 io.on('connection',(socket) => {
     console.log('a user connected')
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+      });
+
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
 })
 
 server.listen(process.env.PORT,()=>{
---
+++ Chat_Frontend/src/main.ts
@@ -0,0 +1,35 @@
+import './style.css'
+import { io } from 'socket.io-client'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+<ul id="messages"></ul>
+<form id="form" action="">
+  <input id="input" autocomplete="off" /><button>Send</button>
+</form>
+`
+
+
+
+const socket = io("http://localhost:4000", {
+  // path: ""
+})
+
+let messages = document.getElementById('messages') as HTMLUListElement;
+let form = document.getElementById('form') as HTMLFormElement
+let input = document.getElementById('input') as HTMLInputElement
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  if (input?.value) {
+    socket.emit('chat message', input.value);
+    input.value = '';
+  }
+})
+
+socket.on('chat message', function(msg) {
+  let item = document.createElement('li');
+  item.textContent = msg;
+  messages.appendChild(item);
+  window.scrollTo(0, document.body.scrollHeight);
+});
+
